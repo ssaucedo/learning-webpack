4b4042d098856e9d1f83489d65403899e761b2fa
Santiago Saucedo <santiago.g.saucedo@gmail.com>
2017-07-14 22:44:34 -0300

    Remove unnecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-var CustomAssetPlugin = require('./CustomAssetPlugin');
-var AppendCodePlugin = require('./AppendCodePlugin');
-var LogHookPlugin = require('./LogHookPlugin');
-
 
 module.exports = {
   entry: {
@@ -14,11 +9,7 @@ module.exports = {
     plugins: [
       new HtmlWebpackPlugin({
       title: 'Output Management'
-    }),
-    //new CleanWebpackPlugin(['dist']),
-    new CustomAssetPlugin({assetName: 'asset-name.txt', content: 'Asset content'}),
-    new AppendCodePlugin(),
-    ],
+    })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
